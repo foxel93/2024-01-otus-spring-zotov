9f61698fa2436802158aa5034c786b3172abb70a
vzotov <vzotov@ozon.ru>
2024-02-01 17:27:50 +0300

    add 5 tests

--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,6 +1,12 @@
 package ru.otus.hw.service;
 
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
@@ -9,8 +15,54 @@ public class TestServiceImpl implements TestService {
 
     @Override
     public void executeTest() {
-        ioService.printLine("");
-        ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+        var context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        var questionDao = context.getBean(QuestionDao.class);
+        var scanner = new Scanner(System.in);
+
+        ioService.printLine("\nPlease answer the questions below\n");
+
+        var result = new TreeMap<String, Boolean>();
+        questionDao.findAll().forEach(question -> {
+            printQuestion(question);
+
+            ioService.printLine("Input answer number: ");
+            var userAnswerPos = scanner.next().trim();
+            var expectedPos = Integer.toString(correctAnswerPosition(question));
+
+            result.put(question.text(), expectedPos.equals(userAnswerPos));
+        });
+        printResult(result);
+    }
+
+    private void printQuestion(Question question) {
+        ioService.printLine(question.text());
+        for (int i = 0; i < question.answers().size(); i++) {
+            ioService.printFormattedLine("\t[%d]: %s", i, question.answers().get(i).text());
+        }
+    }
+
+    private void printResult(Map<String, Boolean> result) {
+        ioService.printLine("Result:\n");
+
+        int score = 0;
+        for (var entry : result.entrySet()) {
+            var answer = entry.getKey();
+            var status = entry.getValue();
+
+            ioService.printLine("- Answer: " + answer);
+            ioService.printLine("  Status: " + (status ? "correct" : "failed"));
+
+            score += (status ? 1 : 0);
+        }
+        ioService.printFormattedLine("\nScore: %d/%d", score,result.size());
+    }
+
+    private int correctAnswerPosition(Question question) {
+        for (int i = 0; i < question.answers().size(); i++) {
+            if (question.answers().get(i).isCorrect()) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
