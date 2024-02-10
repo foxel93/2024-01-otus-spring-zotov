7833946bac4670c391699cc07338c5970f3aa7ca
vzotov <vzotov@ozon.ru>
2024-02-10 13:33:06 +0300

    add input answer

--- hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
@@ -23,22 +24,34 @@ public class TestServiceImpl implements TestService {
         var testResult = new TestResult(student);
 
         for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+            printQuestion(question);
+
+            var expectedAnswerId = expectedAnswerId(question);
+            var actualAnswerId = actualAnswerId(question);
+
+            var isAnswerValid = expectedAnswerId == actualAnswerId;
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
     }
 
-//    @Override
-//    public void executeTest() {
-//        ioService.printLine("\nPlease answer the questions below\n");
-//        questionDao.findAll().forEach(this::printQuestion);
-//    }
-//
-//    private void printQuestion(Question question) {
-//        ioService.printLine(question.text());
-//        for (int i = 0; i < question.answers().size(); i++) {
-//            ioService.printFormattedLine("\t[%d]: %s", i, question.answers().get(i).text());
-//        }
-//    }
+    private void printQuestion(Question question) {
+        ioService.printLine(question.text());
+        for (int i = 0; i < question.answers().size(); i++) {
+            ioService.printFormattedLine("\t[%d]: %s", i, question.answers().get(i).text());
+        }
+    }
+
+    private int expectedAnswerId(Question question) {
+        for (int i = 0; i < question.answers().size(); i++) {
+            if (question.answers().get(i).isCorrect()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int actualAnswerId(Question question) {
+        return ioService.readIntForRange(0, question.answers().size() - 1, "Invalid answer");
+    }
 }
