fcf80cd66691bce65bfe2916629ee811c878f0e7
vzotov <vzotov@ozon.ru>
2024-02-10 13:35:23 +0300

    add test

--- hw02/src/test/java/ru/otus/hw/service/TestServiceImpTest.java
+++ hw02/src/test/java/ru/otus/hw/service/TestServiceImpTest.java
@@ -5,15 +5,17 @@ import static org.mockito.Mockito.times;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Student;
 
 public class TestServiceImpTest {
-//    @Test
-//    void executeTest_question_callCount() {
-//        var ioServiceMock = Mockito.mock(IOService.class);
-//        var questionDaoMock = Mockito.mock(QuestionDao.class);
-//        var service = new TestServiceImpl(ioServiceMock, questionDaoMock);
-//
-//        service.executeTest();
-//        Mockito.verify(questionDaoMock, times(1)).findAll();
-//    }
+    @Test
+    void executeTest_question_callCount() {
+        var ioServiceMock = Mockito.mock(IOService.class);
+        var questionDaoMock = Mockito.mock(QuestionDao.class);
+        var service = new TestServiceImpl(ioServiceMock, questionDaoMock);
+        var student = new Student("some_name", "some_surname");
+
+        service.executeTestFor(student);
+        Mockito.verify(questionDaoMock, times(1)).findAll();
+    }
 }
