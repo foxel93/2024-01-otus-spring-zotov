5d75353097804b19329114c5fe38ceb178fd83cc
vzotov <vzotov@ozon.ru>
2024-03-17 13:01:55 +0300

    comment book fetch lazy

--- hw06/src/main/java/ru/otus/hw/models/Comment.java
+++ hw06/src/main/java/ru/otus/hw/models/Comment.java
@@ -2,6 +2,7 @@ package ru.otus.hw.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,7 +28,7 @@ public class Comment {
     @Column(name = "text")
     private String text;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id")
     private Book book;
 }
