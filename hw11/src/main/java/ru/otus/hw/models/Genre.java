b69ce999ee737892e21eb5e64a20041b4a2df4ba
vzotov <vzotov@ozon.ru>
2024-05-18 12:17:48 +0300

    models

--- hw11/src/main/java/ru/otus/hw/models/Genre.java
+++ hw11/src/main/java/ru/otus/hw/models/Genre.java
@@ -1,27 +1,23 @@
 package ru.otus.hw.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "genres")
+@AllArgsConstructor(onConstructor_ = @PersistenceCreator)
+@Document(collection = "genres")
 public class Genre {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private String id;
 
-    @Column(name = "name")
+    @Field(name = "name")
     private String name;
 }
