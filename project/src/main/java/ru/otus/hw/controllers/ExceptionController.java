753339d7b487ad0563be7205889dc28b1c025279
vzotov <vzotov@ozon.ru>
2024-08-03 22:34:35 +0300

    ControllerAdvice -> RestControllerAdvice

--- project/src/main/java/ru/otus/hw/controllers/ExceptionController.java
+++ project/src/main/java/ru/otus/hw/controllers/ExceptionController.java
@@ -4,11 +4,11 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.otus.hw.exceptions.NotFoundException;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Log4j2
 public class ExceptionController {
     @ExceptionHandler(NotFoundException.class)
